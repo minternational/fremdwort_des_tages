0fb1043912c027b12ba388d8c81261c2eca03d78
Christopher <54006760+minternational@users.noreply.github.com>
2022-12-27 13:23:29 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,8 @@
+[
+  {
+    id: 1,
+    word: 'Determinismus',
+    description:
+      'Determinismus ist die Philosophie, dass alle Ereignisse in der Welt von vorherbestimmten Ursachen abhängen und somit unveränderlich sind.',
+  },
+];
---
+++ script.js
@@ -0,0 +1,12 @@
+import data from data.js;
+
+const word = document.getElementById('word');
+const description = document.getElementById('description');
+const button = document.getElementById('button');
+
+
+word.innerHTML = 
+
+const randomWord = () => {
+  data.
+}
