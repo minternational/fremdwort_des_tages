2657f9dc057601c52fd2bae0bb0fe2f7a7ae25a5
Christopher <54006760+minternational@users.noreply.github.com>
2022-12-27 13:23:57 +0100

    Delete data.js

--- data.js
+++
@@ -1,8 +0,0 @@
-[
-  {
-    id: 1,
-    word: 'Determinismus',
-    description:
-      'Determinismus ist die Philosophie, dass alle Ereignisse in der Welt von vorherbestimmten Ursachen abhängen und somit unveränderlich sind.',
-  },
-];
